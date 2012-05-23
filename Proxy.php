e6185d84f7151663becf10d9cf25ab47700ba2f2
Martyn Atkins <martat@pml.ac.uk>
2012-05-23 08:46:08 +0000

    1st check-in of OpEc Web Visualisation Portal
    git-svn-id: http://rsg.pml.ac.uk/intranet/svnroots/opec/WP6/OpEcVis@5 88df7874-9788-482e-997b-05ce3c9f3a1b

---
+++ Proxy.php
@@ -0,0 +1,55 @@
+<?php
+// PHP Proxy
+// Responds to both HTTP GET and POST requests
+//
+// Author: Abdul Qabiz
+// March 31st, 2006
+//
+
+// Get the url of to be proxied
+// Is it a POST or a GET?
+$url = ($_POST['url']) ? $_POST['url'] : $_GET['url'];
+$headers = ($_POST['headers']) ? $_POST['headers'] : $_GET['headers'];
+$mimeType =($_POST['mimeType']) ? $_POST['mimeType'] : $_GET['mimeType'];
+
+
+//Start the Curl session
+$session = curl_init($url);
+
+// If it's a POST, put the POST data in the body
+if ($_POST['url']) {
+$postvars = '';
+while ($element = current($_POST)) {
+$postvars .= key($_POST).'='.$element.'&';
+next($_POST);
+}
+curl_setopt ($session, CURLOPT_POST, true);
+curl_setopt ($session, CURLOPT_POSTFIELDS, $postvars);
+}
+
+// Don't return HTTP headers. Do return the contents of the call
+curl_setopt($session, CURLOPT_HEADER, ($headers == "true") ? true : false);
+
+curl_setopt($session, CURLOPT_FOLLOWLOCATION, true);
+//curl_setopt($ch, CURLOPT_TIMEOUT, 4);
+curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+
+// Make the call
+$response = curl_exec($session);
+$content_type = curl_getinfo( $session, CURLINFO_CONTENT_TYPE );
+
+if ($mimeType != "")
+{
+// The web service returns XML. Set the Content-Type appropriately
+header("Content-Type: ".$mimeType);
+}
+else
+{
+        header("Content-Type: ".$content_type);
+}
+
+echo $response;
+
+curl_close($session);
+
+?>
