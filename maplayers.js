bb00b9772f1ba6d64abd31285598a803d1430b26
Martyn Atkins <martat@pml.ac.uk>
2012-07-12 17:02:27 +0000

    git-svn-id: http://rsg.pml.ac.uk/intranet/svnroots/opec/WP6/OpEcVis@42 88df7874-9788-482e-997b-05ce3c9f3a1b

--- maplayers.js
+++ maplayers.js
@@ -14,6 +14,9 @@ OpenLayers.Layer.prototype.temporal = false;
 // Holds cached date-times as array of ISO8601 strings for each layer based on data availability
 OpenLayers.Layer.prototype.DTCache = [];
 
+// Is the layer selected for display in the GUI or not
+OpenLayers.Layer.prototype.selected = false;
+
 // Layer function to create it's date-time cache based on a JSON cacheFile
 // This is an asynchronous AJAX load of the JSON data
 OpenLayers.Layer.prototype.createDateCache = function(cacheFile){
@@ -23,38 +26,81 @@ OpenLayers.Layer.prototype.createDateCache = function(cacheFile){
 	});		
 };
 
-// Map function to filter of all map layers with date-time dependencies to an ISO8601 format date
+// Function which looks for a date within a layer.
+// The date passed is in the format yyyy-mm-dd or is an empty string
+// Returns the 1st date if there's a match or null if not.
+OpenLayers.Layer.prototype.matchDate = function (thedate){
+	var thelayer = this;
+	var filtArray = $.grep(thelayer.DTCache, function(dt, i) {
+		var datePart = dt.substring(0, 10);
+		return (datePart == thedate);
+	});
+	if (filtArray.length>0){
+		return filtArray[0];
+	}
+	else{
+		return null;
+	}
+}
+
+/*// Map function to filter of all map layers with date-time dependencies to an ISO8601 format date
 OpenLayers.Map.prototype.filterLayersByDate = function(isoDates){
 	var themap = this;
 	var dates = isoDates;
 	$.each(themap.layers, function(index, value) {
 		var layer = value;
+		var display = false;
 		if(layer.temporal){
-			layer.mergeNewParams({time: '0000-00-00T00:00:00.000Z'});
-			var hit = false;
 			$.each(dates, function(index, value) {
 				var d = value;
 				// If we get a match, filter the layer to the exact date-time
 				if($.inArray(d,layer.DTCache) > -1){
 					layer.mergeNewParams({time: d});
+					display = true;
+					layer.setVisibility(layer.selected)
 					// DEBUG
-					if (layer.visibility){
-						console.info('Layer ' + layer.name + ' data available for date-time ' + d + '. Displaying layer.');
-					}
+					console.info('Layer ' + layer.name + ' data available for date-time ' + d + '. Displaying layer: ' + layer.selected);
 				}
 			});
 		}
 	});		
+};*/
+
+// Map function to filter of all map layers with date-time dependencies to an yyyy-mm-dd format date
+OpenLayers.Map.prototype.filterLayersByDate = function(dateText, inst){
+	var themap = this;
+	var thedate = new Date(inst.selectedYear, inst.selectedMonth, inst.selectedDay);
+	$.each(themap.layers, function(index, value) {
+		var layer = value;
+		themap.selectLayer(value, thedate);
+	});		
 };
 
-// Map function which returns availability (boolean) of data for the given date across all layers
-OpenLayers.Map.prototype.allowedDays = function(date) {
+// Select the given layer on the Map method based on JavaScript date input 
+OpenLayers.Map.prototype.selectLayer = function(lyr, thedate){
+	var layer = lyr;
+	if(layer.temporal && thedate){
+		var uidate = ISODateString(thedate);
+		var mDate = layer.matchDate(uidate);
+		if(mDate){
+			layer.mergeNewParams({time: mDate});
+			layer.setVisibility(layer.selected);
+			// DEBUG
+			console.info('Layer ' + layer.name + ' data available for date-time ' + mDate + '. Displaying layer: ' + layer.selected);
+		}
+		else{
+			layer.setVisibility(false);
+			// DEBUG
+			console.info('Layer ' + layer.name + ' no data available for date-time ' + mDate + '. Not displaying layer.');
+		}
+	}
+};
+
+// Map function which returns availability (boolean) of data for the given JavaScript date across all layers
+OpenLayers.Map.prototype.allowedDays = function(thedate) {
 	var themap = this;
-	var m = date.getMonth() + 1, d = date.getDate(), y = date.getFullYear();
-	if(m < 10) { m = '0' + m; }
-	if(d < 10) { d = '0' + d; }
-	var uidate = y + '-' + m + '-' + d;
-	// Flter the datetime array to see if it matches the date using jQuery grep utility
+	var uidate = ISODateString(thedate);
+	// Filter the datetime array to see if it matches the date using jQuery grep utility
 	var filtArray = $.grep(themap.enabledDays, function(dt, i) {
 		var datePart = dt.substring(0, 10);
 		return (datePart == uidate);
@@ -68,7 +114,7 @@ OpenLayers.Map.prototype.allowedDays = function(date) {
 	}
 }
 
-// Map function which handles change of view date and filters available date-times to this date
+/*// Map function which handles change of view date and filters available date-times to this date
 OpenLayers.Map.prototype.changeViewDate = function(dateText, inst) {
 	var themap = this;
 	var d = inst.selectedDay;
@@ -83,4 +129,4 @@ OpenLayers.Map.prototype.changeViewDate = function(dateText, inst) {
 		return (datePart == uidate);
 	});
 	themap.filterLayersByDate(filtArray);
-}
+}*/
