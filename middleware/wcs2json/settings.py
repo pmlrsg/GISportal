045b9976bbffc829de145da848a4d6abb8a37ab2
Martyn Atkins <martat@pml.ac.uk>
2012-10-16 13:28:47 +0000

    Tidied up settings files
    git-svn-id: http://rsg.pml.ac.uk/intranet/svnroots/opec/WP6/OpEcVis@186 88df7874-9788-482e-997b-05ce3c9f3a1b

--- middleware/wcs2json/settings.py
+++ middleware/wcs2json/settings.py
@@ -1,4 +1,4 @@
 DEBUG = False
 DEBUG_WITH_APTANA = False
-PATH = '/var/www/html/rbb/opecvis/middleware/wcs2json' #Rob
-#PATH = '/var/www/html/opecvis/middleware/wcs2json' #Martyn
+# This path needs changing according to local server configuration
+PATH = '/var/www/html/rbb/opecvis/middleware/wcs2json'
