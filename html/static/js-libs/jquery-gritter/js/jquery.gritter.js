af999dd57e9a365fab4732bef3773c80a35ce21e
Robert Brigden <rbb@pml.ac.uk>
2013-02-01 12:21:05 +0000

    Added mustache lib for templates, updated jquery-gritter jsdoc, added lib folder and two libs, jasmine and plovr.
    git-svn-id: http://rsg.pml.ac.uk/intranet/svnroots/opec/WP6/OpEcVis@273 88df7874-9788-482e-997b-05ce3c9f3a1b

--- html/static/js-libs/jquery-gritter/js/jquery.gritter.js
+++ html/static/js-libs/jquery-gritter/js/jquery.gritter.js
@@ -86,9 +86,9 @@
 		/**
 		* Add a gritter notification to the screen
 		* @param {Object} params The object that contains all the options for drawing the notification
-		* @return {Integer} The specific numeric id to that gritter notification
+		* @return {number} The specific numeric id to that gritter notification
 		*/
-		add: function(params){
+		add: function(params) {
 			// Handle straight text
 			if(typeof(params) == 'string'){
 				params = {text:params};
@@ -206,9 +206,9 @@
 		/**
 		* If we don't have any more gritter notifications, get rid of the wrapper using this check
 		* @private
-		* @param {Integer} unique_id The ID of the element that was just deleted, use it for a callback
+		* @param {number} unique_id The ID of the element that was just deleted, use it for a callback
 		* @param {Object} e The jQuery element that we're going to perform the remove() action on
-		* @param {Boolean} manual_close Did we close the gritter dialog with the (X) button
+		* @param {boolean} manual_close Did we close the gritter dialog with the (X) button
 		*/
 		_countRemoveWrapper: function(unique_id, e, manual_close){
 			
@@ -227,13 +227,13 @@
 		* Fade out an element after it's been on the screen for x amount of time
 		* @private
 		* @param {Object} e The jQuery element to get rid of
-		* @param {Integer} unique_id The id of the element to remove
+		* @param {number} unique_id The id of the element to remove
 		* @param {Object} params An optional list of params to set fade speeds etc.
-		* @param {Boolean} unbind_events Unbind the mouseenter/mouseleave events if they click (X)
+		* @param {boolean} unbind_events Unbind the mouseenter/mouseleave events if they click (X)
 		*/
-		_fade: function(e, unique_id, params, unbind_events){
+		_fade: function(e, unique_id, parameters, unbind_events){
 
-			var params = params || {},
+			var params = parameters || {},
 				fade = (typeof(params.fade) != 'undefined') ? params.fade : true,
 				fade_out_speed = params.speed || this.fade_out_speed,
 				manual_close = unbind_events;
@@ -269,7 +269,7 @@
 		* Perform actions based on the type of bind (mouseenter, mouseleave) 
 		* @private
 		* @param {Object} e The jQuery element
-		* @param {String} type The type of action we're performing: mouseenter or mouseleave
+		* @param {string} type The type of action we're performing: mouseenter or mouseleave
 		*/
 		_hoverState: function(e, type){
 			
@@ -296,18 +296,18 @@
 		
 		/**
 		* Remove a specific notification based on an ID
-		* @param {Integer} unique_id The ID used to delete a specific notification
+		* @param {number} group The group of the specific notification
 		* @param {Object} params A set of options passed in to determine how to get rid of it
 		* @param {Object} e The jQuery element that we're "fading" then removing
-		* @param {Boolean} unbind_events If we clicked on the (X) we set this to true to unbind mouseenter/mouseleave
+		* @param {boolean} unbind_events If we clicked on the (X) we set this to true to unbind mouseenter/mouseleave
 		*/
 		removeSpecific: function(group, params, e, unbind_events){
 			
-			unique_id = this._groups[group].uid.shift();
+			var unique_id = this._groups[group].uid.shift();
          this._groups[group].count--;
 			
 			if(!e){
-				var e = $('#gritter-item-' + unique_id);
+				e = $('#gritter-item-' + unique_id);
 			}
 
 			// We set the fourth param to let the _fade function know to 
@@ -320,7 +320,7 @@
 		* If the item is fading out and we hover over it, restore it!
 		* @private
 		* @param {Object} e The HTML element to remove
-		* @param {Integer} unique_id The ID of the element
+		* @param {number} unique_id The ID of the element
 		*/
 		_restoreItemIfFading: function(e, unique_id){
 			
@@ -337,7 +337,7 @@
 		*/
 		_runSetup: function(){
 		
-			for(opt in $.gritter.options){
+			for(var opt in $.gritter.options){
 				this[opt] = $.gritter.options[opt];
 			}
 			this._is_setup = 1;
@@ -348,7 +348,7 @@
 		* Set the notification to fade out after a certain amount of time
 		* @private
 		* @param {Object} item The HTML element we're dealing with
-		* @param {Integer} unique_id The ID of the element
+		* @param {number} unique_id The ID of the element
 		*/
 		_setFadeTimer: function(e, unique_id){
 			
@@ -381,9 +381,9 @@
 		/**
 		* An extremely handy PHP function ported to JS, works well for templating
 		* @private
-		* @param {String/Array} search A list of things to search for
-		* @param {String/Array} replace A list of things to replace the searches with
-		* @return {String} sa The output
+		* @param {(string|Array)} search A list of things to search for
+		* @param {(string|Array)} replace A list of things to replace the searches with
+		* @return {string} sa The output
 		*/  
 		_str_replace: function(search, replace, subject, count){
 		
