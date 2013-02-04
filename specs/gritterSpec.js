8a1151b6b4fd2f78224de5abf7f885cef67265dd
Robert Brigden <rbb@pml.ac.uk>
2013-02-04 10:26:58 +0000

    Added Jasmine for unit testing.
    git-svn-id: http://rsg.pml.ac.uk/intranet/svnroots/opec/WP6/OpEcVis@274 88df7874-9788-482e-997b-05ce3c9f3a1b

---
+++ specs/gritterSpec.js
@@ -0,0 +1,9 @@
+describe("opec.gritter", function() {
+   var g = gritter;
+
+   it("Shows message", function() {
+      g.showNotification("welcomeTutorial", null);
+      
+      expect()
+   })
+})
