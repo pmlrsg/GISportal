338647f777556bf6586861b0be2d891152ed8ff4
Shane Hudson <shane@shanehudson.net>
2014-03-18 15:16:35 +0000

    Adding settings to config
    git-svn-id: http://rsg.pml.ac.uk/intranet/svnroots/opec/WP6/OpEcVis@567 88df7874-9788-482e-997b-05ce3c9f3a1b

---
+++ config_examples/settings.py
@@ -0,0 +1,19 @@
+DEBUG = True
+DEBUG_WITH_APTANA = False
+
+JSON_SORT_KEYS = False
+
+SECRET_KEY = 'p7i0-22@0eheas^kzw3=1qfug_x+5)5)8u4v=2iyiwwx1eh)37'
+
+OPENID_FOLDER = '/home/rsgadmin/cache/opec/openID'
+DATABASE_URI = 'sqlite:///' + OPENID_FOLDER + '/user_storage.db'
+
+
+LOG_LEVEL = "DEBUG"
+# This path needs changing according to local server configuration
+# Leave as empty if you want the log to be in the same place as the .wsgi
+LOG_PATH = ''
+
+# The error level of logging into the database
+# E for errors only or W for warnings too
+ERROR_LEVEL = "W"
