ca9c4673b91d1f5ea831c844f3151cfddbff6372
Shane Hudson <shane@shanehudson.net>
2014-03-19 15:09:26 +0000

    Changing file names

--- src/opec.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-   
-   // IE 9 Fix for missing console
-   if (!window.console) window.console = {};
-   if (!window.console.log) window.console.log = function () { };
-   
-   gisportal.main();
-});
