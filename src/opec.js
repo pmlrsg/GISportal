ee9ad859310cc8dc1e93ebbe6bcb89eb4850958a
Shane Hudson <shane@shanehudson.net>
2014-03-28 18:57:44 +0000

    Revert "Forgot to svn rm some stuff."
    This reverts commit 9378c4699f8dbf22613a26a3f35ab43e09eb841a.

---
+++ src/opec.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+   
+   // IE 9 Fix for missing console
+   if (!window.console) window.console = {};
+   if (!window.console.log) window.console.log = function () { };
+   
+   opec.main();
+});
