febe4ca79c4dc208d0a2ea7203ed6a68c83356ac
Shane Hudson <shane@shanehudson.net>
2014-05-19 12:20:49 +0100

    Adding config.js

---
+++ config_examples/config.js
@@ -0,0 +1,22 @@
+/*------------------------------------*\
+    Configuration
+    This file is for the configuration
+    of the GIS Portal.
+
+    browseCategories - Used to define
+    which categories to be shown in the
+    browse panel. This is currently
+    limited to 2.
+\*------------------------------------*/
+
+
+gisportal.config = {
+   browseCategories : {
+      "Ecosystem_Element" : "Ecosystem",
+      "MSFD" : "MSFD"
+   },
+   popularIndicators : [
+      "Z6c", "max_Chl_surf", "MicroZoo1",
+      "NPP", "Oxygen"
+   ]
+};
