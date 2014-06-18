83516bd8c333906b1eff23abde45e2c01c97efa7
Ben Calton <bac@pml.ac.uk>
2014-06-18 15:05:52 +0100

    bac: portal settings files not included in git

---
+++ middleware/portalflask/settings.py
@@ -0,0 +1,19 @@
+DEBUG = True
+DEBUG_WITH_APTANA = False
+
+JSON_SORT_KEYS = False
+
+SECRET_KEY = 'HWnbQHpVMAVEBvmtK9mIvem9az8j3RfMVNI4BZ8koEN1jQhfFm9DbM4Q'
+
+OPENID_FOLDER = '/home/rsgadmin/wci.earth2observe.eu-portal/openID'
+DATABASE_URI = 'sqlite:///' + OPENID_FOLDER + '/user_storage.db'
+
+
+LOG_LEVEL = "DEBUG"
+# This path needs changing according to local server configuration
+# Leave as empty if you want the log to be in the same place as the .wsgi
+LOG_PATH = ''
+
+# The error level of logging into the database
+# E for errors only or W for warnings too
+ERROR_LEVEL = "W"
