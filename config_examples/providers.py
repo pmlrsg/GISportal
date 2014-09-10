e41411b8394ff516303e33ca2643e3290087f9dc
Kieran Jones <kij@pmpc1310.npm.ac.uk>
2014-09-10 10:03:23 +0100

    Made the loading wheel bigger/nice.

--- config_examples/providers.py
+++ config_examples/providers.py
@@ -5,26 +5,32 @@ providers ={
   },
   "Cefas": {
     "logo": "img/cefas.png",
-    "website": "http://www.cefas.defra.gov.uk/"
+    "website": "http://www.cefas.defra.gov.uk/",
+    "email": "Jonathan.Beecham@cefas.co.uk"
   }, 
   "DMI": {
     "logo": "img/dmi.png",
-    "website": "http://www.dmi.dk/en/vejr/"
+    "website": "http://www.dmi.dk/en/vejr/",
+    "email": "zw@dmi.dk"
   },
   "HCMR": {
     "logo": "img/hcmr.png",
-    "website": "http://innovator.ath.hcmr.gr/newhcmr1/"
+    "website": "http://innovator.ath.hcmr.gr/newhcmr1/",
+    "email": "ktsiaras@hcmr.gr"
   },
   "IMS-METU": {
     "logo": "img/metu.png",
-    "website": "http://www.ims.metu.edu.tr/"
+    "website": "http://www.ims.metu.edu.tr/",
+    "email": "sinan.arkin@ims.metu.edu.tr"
   },
   "OGS": {
     "logo": "img/ogs.png",
-    "website": "http://www.ogs.trieste.it/"
+    "website": "http://www.ogs.trieste.it/",
+    "email": "gcossarini@ogs.trieste.it"
   },
   "PML": {
     "logo": "img/pml.png",
-    "website": "http://www.pml.ac.uk/"
+    "website": "http://www.pml.ac.uk/",
+    "email": "s.ciavatta@pml.ac.uk"
   }
 }
