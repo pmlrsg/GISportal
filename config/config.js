f54abb0f52cd373712ea758841ed2a25f29d0ac3
Ben Calton <bac@pml.ac.uk>
2014-10-31 14:19:05 +0000

    Config settings update

--- config/config.js
+++ config/config.js
@@ -9,37 +9,35 @@
     limited to 2.
 \*------------------------------------*/
 
-
-// gisportal.config = {
-//    browseCategories : {
-//       "Ecosystem_Element" : "Ecosystem",
-//       "MSFD" : "MSFD"
-//    },
-//    popularIndicators : [
-//       "Z6c", "max_Chl_surf", "MicroZoo1",
-//       "NPP", "Oxygen"
-//    ]
-// };
-
 gisportal.config = {
+   siteMode: "development", //(development|production)
    browseCategories : {
       "indicator_type" : "Indicator Type",
       "data_provider" : "Data Provider",
       "region" : "Region"
    },
-   popularIndicators : [
-      "Rainfall Rate",
-      "Near surface specific humidity",
-      "Surface incident longwave radiation",
-      "Surface incident shortwave radiation"
-   ],
+   //  Skip start screen only is the user has a saved state, requires T&C
+   autoResumeSavedState: false,
+   // Always skip the welcome page, also skips T&C
+   skipWelcomePage: false,
+   // Do we require terms and conditions agreement to use the port
+   requiresTermsAndCondictions: true,
    paths: {
     graphServer: 'https://wci.earth2observe.eu/plotting',
     middlewarePath: '/service'
    },
+   countryBorder : {
+      'defaultLayer' : 'countries_all_white',      // (countries_all_white|countries_all_black|countries_all_blue)
+      'alwaysVisible' : true                      // (true|false)  > If true the defaultLayer will be visible at page load
+   },
+   defaultBaseMap: 'EOX',
 
    // Should layers auto scale by default
    autoScale: true,
    requiresTermsAndCondictions: true,
+   
+   // Deny access to older browsers
+   // none=Allow, advisory=Tell them only, strict=Stop them
+   browserRestristion: "strict" //(none|advisory|strict)
    
 };
