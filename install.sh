05179be3ea047da2d568d6825ee6105adca9775f
Oliver Standeven <ols@pml.ac.uk>
2016-02-11 14:10:05 +0000

    Made sure the install script doesn't overwrite any config files.

--- install.sh
+++ install.sh
@@ -22,50 +22,66 @@ read -p "Do you wish to setup a domain now? (y/n)?" -n 1 choice #-
 case "$choice" in 
   y|Y ) echo; getDomainInfo;;
   n|N ) domain="/" ;;
-  * ) echo; echo "Please choose y or n"; echo;;
+  * ) echo; echo "Please choose y or n";;
 esac
 done
 
-mkdir -p config/site_settings/"$domain";
-mkdir -p config/site_settings/layers;
+if [ ! -e config/site_settings/"$domain" ]
+	then
+		mkdir -p config/site_settings/"$domain";
+fi
 
-echo Your domain folder has been created
+if [ ! -e config/site_settings/layers ]
+	then
+		mkdir -p config/site_settings/layers;
+fi
 
-echo "GLOBAL.config['$domain'] = {">>config/site_settings/"$domain"/config-server.js
-echo "   // Application settings">>config/site_settings/"$domain"/config-server.js
-echo "   app: {">>config/site_settings/"$domain"/config-server.js
-echo "      // the port that the application will run on">>config/site_settings/"$domain"/config-server.js
-echo "      port: 6789,">>config/site_settings/"$domain"/config-server.js
-echo "   },">>config/site_settings/"$domain"/config-server.js
-echo "   // redis connection settings">>config/site_settings/"$domain"/config-server.js
-echo "   redisURL: 'http$ssl://localhost:6379/',">>config/site_settings/"$domain"/config-server.js
-echo "   // OAuth2 settings from Google, plus others if applicable">>config/site_settings/"$domain"/config-server.js
-echo "   auth: {">>config/site_settings/"$domain"/config-server.js
-echo "      google: {">>config/site_settings/"$domain"/config-server.js
-echo "         scope : 'https://www.googleapis.com/auth/userinfo.email',">>config/site_settings/"$domain"/config-server.js
-echo "         clientid : '$clientid',">>config/site_settings/"$domain"/config-server.js
-echo "         clientsecret : '$clientsecret',">>config/site_settings/"$domain"/config-server.js
-echo "         callback : 'http$ssl://$domain/app/user/auth/google/callback',">>config/site_settings/"$domain"/config-server.js
-echo "         prompt: 'select_account'">>config/site_settings/"$domain"/config-server.js
-echo "      }">>config/site_settings/"$domain"/config-server.js
-echo "   },">>config/site_settings/"$domain"/config-server.js
-echo "   // session settings">>config/site_settings/"$domain"/config-server.js
-echo "   session : {">>config/site_settings/"$domain"/config-server.js
-echo "      // ssssh! it's secret (any randon string to keep prying eyes from seeing the content of the cookie)">>config/site_settings/"$domain"/config-server.js
-echo "      secret : '$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 50 | head -n 1)',">>config/site_settings/"$domain"/config-server.js
-echo "      // the age in seconds that the cookie should persist; 0 == session cookie that expires when the browser is closed">>config/site_settings/"$domain"/config-server.js
-echo "      age : 0">>config/site_settings/"$domain"/config-server.js
-echo "   },">>config/site_settings/"$domain"/config-server.js
-echo "   admins:[">>config/site_settings/"$domain"/config-server.js
-echo "      '$admin_email'">>config/site_settings/"$domain"/config-server.js
-echo "   ]">>config/site_settings/"$domain"/config-server.js
-echo "}">>config/site_settings/"$domain"/config-server.js
+if [ $domain != "/" ]
+	then
+		echo "GLOBAL.config['$domain'] = {">>config/site_settings/"$domain"/config-server.js
+		echo "   // Application settings">>config/site_settings/"$domain"/config-server.js
+		echo "   app: {">>config/site_settings/"$domain"/config-server.js
+		echo "      // the port that the application will run on">>config/site_settings/"$domain"/config-server.js
+		echo "      port: 6789,">>config/site_settings/"$domain"/config-server.js
+		echo "   },">>config/site_settings/"$domain"/config-server.js
+		echo "   // redis connection settings">>config/site_settings/"$domain"/config-server.js
+		echo "   redisURL: 'http$ssl://localhost:6379/',">>config/site_settings/"$domain"/config-server.js
+		echo "   // OAuth2 settings from Google, plus others if applicable">>config/site_settings/"$domain"/config-server.js
+		echo "   auth: {">>config/site_settings/"$domain"/config-server.js
+		echo "      google: {">>config/site_settings/"$domain"/config-server.js
+		echo "         scope : 'https://www.googleapis.com/auth/userinfo.email',">>config/site_settings/"$domain"/config-server.js
+		echo "         clientid : '$clientid',">>config/site_settings/"$domain"/config-server.js
+		echo "         clientsecret : '$clientsecret',">>config/site_settings/"$domain"/config-server.js
+		echo "         callback : 'http$ssl://$domain/app/user/auth/google/callback',">>config/site_settings/"$domain"/config-server.js
+		echo "         prompt: 'select_account'">>config/site_settings/"$domain"/config-server.js
+		echo "      }">>config/site_settings/"$domain"/config-server.js
+		echo "   },">>config/site_settings/"$domain"/config-server.js
+		echo "   // session settings">>config/site_settings/"$domain"/config-server.js
+		echo "   session : {">>config/site_settings/"$domain"/config-server.js
+		echo "      // ssssh! it's secret (any randon string to keep prying eyes from seeing the content of the cookie)">>config/site_settings/"$domain"/config-server.js
+		echo "      secret : '$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 50 | head -n 1)',">>config/site_settings/"$domain"/config-server.js
+		echo "      // the age in seconds that the cookie should persist; 0 == session cookie that expires when the browser is closed">>config/site_settings/"$domain"/config-server.js
+		echo "      age : 0">>config/site_settings/"$domain"/config-server.js
+		echo "   },">>config/site_settings/"$domain"/config-server.js
+		echo "   admins:[">>config/site_settings/"$domain"/config-server.js
+		echo "      '$admin_email'">>config/site_settings/"$domain"/config-server.js
+		echo "   ]">>config/site_settings/"$domain"/config-server.js
+		echo "}">>config/site_settings/"$domain"/config-server.js
+fi
 
-echo Moving config files...
 
-cp ./config_examples/config-server.js ./config/config-server.js
-cp ./config_examples/base_config.js ./config/base_config.js
-cp ./config_examples/analytics_config.js ./config/analytics_config.js
+if [ ! -e config/config-server.js ]
+	then
+		cp ./config_examples/config-server.js ./config/config-server.js;
+fi
+if [ ! -e config/base_config.js ]
+	then
+		cp ./config_examples/base_config.js ./config/base_config.js;
+fi
+if [ ! -e config/analytics_config.js ]
+	then
+		cp ./config_examples/analytics_config.js ./config/analytics_config.js;
+fi
 
 grunt dev
 node app.js
