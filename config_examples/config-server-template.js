0b30c51ef4dd97ba5f423eed86a3d3d091129316
Ben Calton <bac@pml.ac.uk>
2016-02-22 18:53:01 +0000

    Remove comments from template as the sed'ing removes line breaks and so produces invalid output

--- config_examples/config-server-template.js
+++ config_examples/config-server-template.js
@@ -1,5 +1,4 @@
 GLOBAL.config['DOMAIN_NAME'] = {
-   // OAuth2 settings from Google, plus others if applicable
    auth: {
       google: {
          scope : 'https://www.googleapis.com/auth/userinfo.email',
@@ -9,11 +8,8 @@ GLOBAL.config['DOMAIN_NAME'] = {
          prompt: 'select_account'
       }
    },
-   // session settings
    session : {
-      // ssssh! it's secret (any randon string to keep prying eyes from seeing the content of the cookie)
       secret : 'SECRET',
-      // the age in seconds that the cookie should persist; 0 == session cookie that expires when the browser is closed
       age : 0
    },
    admins:[ADMINISTRATOR]
