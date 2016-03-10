36984e3d2e2541e4d139ea32da84b21f24beca26
Olly Clements <olcl@pml.ac.uk>
2016-03-10 17:09:28 +0000

    added depth to all options and a new file extractor for returning the fielname

--- extraction_utils/wcs_raw_extraction.py
+++ extraction_utils/wcs_raw_extraction.py
@@ -59,6 +59,6 @@ class WCSRawHelper(object):
 			full_url = self.url + self.generateGetCoverageUrl()
 		else:
 			full_url = self.url +'?'+ self.generateGetCoverageUrl()
-		print full_url
+		#print full_url
 		resp = urllib2.urlopen(full_url)
 		return resp
