4f82bd1ca1e4ca3e51eb4e39adad95c6ce0467bb
Olly Clements <olcl@pml.ac.uk>
2016-03-11 12:40:47 +0000

    added depth to scatter

--- analysis_types/scatter_stats.py
+++ analysis_types/scatter_stats.py
@@ -13,12 +13,12 @@ class ScatterStats(object):
 		self.filename2 = filenames[self.variable2]
 
 	def process(self):
-		#print "running basic processing on %s" % self.filename
+		print "running basic processing on %s & %s" % (self.filename1, self.filename2)
 
 		netcdf_file1 = netCDF.Dataset(self.filename1, "r")
 		netcdf_file2 = netCDF.Dataset(self.filename2, "r")
 
-		return basic_scatter(netcdf_file1, self.variable1, netcdf_file2, self.variable2)
+		return json.dumps(basic_scatter(netcdf_file1, self.variable1, netcdf_file2, self.variable2))
 
 
 		#netcdf_variable = netcdf_file[variable]
