812394bffe739ce59bc09edaadcea7c91f6d5888
Oliver Standeven <ols@pml.ac.uk>
2016-03-14 13:06:11 +0000

    Added an error for intervals that dont match

--- src/js/Plot.js
+++ src/js/Plot.js
@@ -579,6 +579,8 @@ gisportal.graphs.Plot =(function(){
       if( _new != old )
          this.emit('plotType-change', { 'new': _new, 'old': old });
 
+      $('.js-create-graph').toggleClass("hidden", false);
+      $('.js-components tr').attr("has-data-in-range", "yes");
       if( _new == 'timeseries'){
          this.setMinMaxComponents(1,10);
          this.setComponentXYText("Left Axis", "Right Axis");
@@ -769,12 +771,33 @@ gisportal.graphs.Plot =(function(){
          var end1 = indicator1.lastDate;
          var start2 = indicator2.firstDate;
          var end2 = indicator2.lastDate;
+
+         var interval1 = indicator1.tags.interval;
+         var interval2 = indicator2.tags.interval;
+
          if(start1 > end2 || start2 > end1){
             $('.graph-date-range-info-li').toggleClass("hidden", true);
             $('.graph-date-range-error-li').toggleClass("hidden", false);
             $('.graph-date-range-error-div').html("<p>When creating a scatter plot the sample times of each indicator must be matching; the indicators you have selected do not overlap in time</p>");
-            $('.js-components tr').attr("has-data-in-range", "no")
+            $('.js-components tr').attr("has-data-in-range", "no");
+            $('.js-create-graph').toggleClass("hidden", true);
             return
+         }else if(!interval1 || !interval2){
+            $('.graph-date-range-info-li').toggleClass("hidden", true);
+            $('.graph-date-range-error-li').toggleClass("hidden", false);
+            $('.graph-date-range-error-div').html("<p>To create a scatter plot the two indicators must be of the same sample frequency; The interval of one of your indicators is undefined</p>");
+            $('.js-components tr').attr("has-data-in-range", "no");
+            $('.js-create-graph').toggleClass("hidden", true);
+            return
+         }else if(interval1 != interval2){
+            $('.graph-date-range-info-li').toggleClass("hidden", true);
+            $('.graph-date-range-error-li').toggleClass("hidden", false);
+            $('.graph-date-range-error-div').html("<p>To create a scatter plot the two indicators must be of the same sample frequency; at the moment you have '" + indicator1.id + "' which is " + interval1 + " and '" + indicator2.id + "' which is " + interval2 + "</p>");
+            $('.js-components tr').attr("has-data-in-range", "no");
+            $('.js-create-graph').toggleClass("hidden", true);
+            return
+         }else{
+            $('.js-create-graph').toggleClass("hidden", true);
          }
       }
       $('.graph-date-range-error-li').toggleClass("hidden", true);
