73d9f10cad94ea063836672c7dce8c7a94529de7
Ben Calton <bac@pml.ac.uk>
2016-03-14 17:05:34 +0000

    Correct example of interactive run

--- docker-install.sh
+++ docker-install.sh
@@ -2,7 +2,7 @@
 #
 #  This runs the initial setup script, so must be run from docker interactively, e.g.
 #
-#  docker run -it pmlrsg/gisportal /app/GISportal/docker-install.sh
+#  docker run -v /usr/share/GISportal:/app/GISportal/config -it pmlrsg/gisportal /app/GISportal/docker-install.sh
 #
 export SOURCE="docker"
 cd /app/GISportal/
