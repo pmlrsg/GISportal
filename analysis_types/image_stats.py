48348cdd1f05d7554168d3a88ae46f38e9bc3627
Olly Clements <olcl@pml.ac.uk>
2016-03-15 18:32:19 +0000

    testing errors in geographic extract

--- analysis_types/image_stats.py
+++ analysis_types/image_stats.py
@@ -17,20 +17,22 @@ class ImageStats(object):
 		#print "running basic processing on %s" % self.filename
 		# create three arrays, 1D lat 1D lon 2D data
 		netcdf_file = netCDF.Dataset(self.filename, "r")
-		variable = netcdf_file.variables[self.variable]
+		variable = np.ma.masked_array(netcdf_file.variables[self.variable])
 		lats = getCoordinateVariable(netcdf_file, "Lat")
 		lons = getCoordinateVariable(netcdf_file, "Lon")
 		time_dim_index = netcdf_file.variables[self.variable].dimensions.index('time')
+		if 'depth' in netcdf_file.variables[self.variable].dimensions:
+			depth_dim_index = netcdf_file.variables[self.variable].dimensions.index('depth')
 		var_list = []
 		lat_list = []
 		lon_list = []
 		print variable.shape
 		if(len(variable.shape) > 3 ):
-			var_list = [[float(x) for x in y] for y in variable[0][0]]
+			var_list = [[float(x) if not np.isinf(x) and not np.isnan(x) else None for x in y  ] for y in np.nanmean(variable, axis=time_dim_index)]
 			lat_list = [float(x) for x in lats]
 			lon_list = [float(x) for x in lons]
 		elif(len(variable.shape) > 2 ):
-			var_list = [[float(x) if not np.isinf(x) else None for x in y  ] for y in np.nanmean(variable, axis=time_dim_index)]
+			var_list = [[float(x) if not np.isinf(x) and not np.isnan(x) else None for x in y  ] for y in np.nanmean(variable, axis=time_dim_index)]
 			#var_list = [[float(x) for x in y] for y in variable[0]]
 			lat_list = [float(x) for x in lats]
 			lon_list = [float(x) for x in lons]
