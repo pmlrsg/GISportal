9fcac13f744896c60955598bb4c381b8ec849a4b
Oliver Standeven <ols@pml.ac.uk>
2016-03-31 11:00:53 +0100

    Added the ability to prevent zoom and drag with views

--- src/js/view.js
+++ src/js/view.js
@@ -7,8 +7,35 @@ gisportal.view.loadView = function(view_name){
          data = JSON.parse(data);
          if(data.bounds){
             var extent = gisportal.reprojectBoundingBox(data.bounds, 'EPSG:4326', gisportal.projection);
-      
             map.getView().fit(extent, map.getSize());
+         }
+         // Gets interactions to be potentially removed
+         var dragPan = null;
+         var wheelZoom = null;
+         map.getInteractions().forEach(function(interaction) {
+            if (interaction instanceof ol.interaction.DragPan) {
+               dragPan = interaction;
+            }else if (interaction instanceof ol.interaction.MouseWheelZoom) {
+               wheelZoom = interaction;
+            }
+         }, this);
+
+         // Adds or removes the dragPan
+         if(dragPan){
+            if(data.noPan){
+               dragPan.setActive(false);
+            }else{
+               dragPan.setActive(true);
+            }
+         }
+
+         // Adds or removes the wheelZoom
+         if(wheelZoom){
+            if(data.noZoom){
+               wheelZoom.setActive(false);
+            }else{
+               wheelZoom.setActive(true);
+            }
          }
       }
    });
