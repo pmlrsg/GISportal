94310ebaa582cf48a486f6370b9a7b80c26edd5f
Oliver Standeven <ols@pml.ac.uk>
2016-06-16 13:35:28 +0100

    changed the name of the global server config file

---
+++ config_examples/global-config-server.js
@@ -0,0 +1,10 @@
+GLOBAL.config = {
+   // the port that the node application will run on
+   appPort: 6789,
+   // redis connection settings
+   redisURL: 'http://localhost:6379/' ,
+   session : {
+      secret : 'SECRET',
+      age : 0
+   }
+}
