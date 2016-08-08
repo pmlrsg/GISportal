f149a4945b5754eba87fb89e427302f3b2522a7f
Oliver Standeven <ols@pml.ac.uk>
2016-08-08 14:09:04 +0100

    Made sure that the graph event listeners were only added once to each button

--- src/js/share.js
+++ src/js/share.js
@@ -24,7 +24,7 @@ gisportal.share.initDOM = function(){
          },
          success: function( data ) {
             if (data) {
-               $('.js-shareurl').val(location.origin + location.pathname + '?state=' + data);
+               $('.js-shareurl').val(location.origin + location.pathname + '?state=' + data).select();
                $('.js-shareurl').focus(function() { $(this).select(); } ).on('mouseup cut paste', function (e) {e.preventDefault();}).on('keydown', function(){$(this).select();});
             }
          }
