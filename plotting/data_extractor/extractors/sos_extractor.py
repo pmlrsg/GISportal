cc5fa6bb0bc69f7ff54de019f00b1314adfbccdc
Ben Calton <bac@pml.ac.uk>
2016-10-03 13:51:56 +0100

    Add new params to SOSExtractor and use them in the request

--- plotting/data_extractor/extractors/sos_extractor.py
+++ plotting/data_extractor/extractors/sos_extractor.py
@@ -7,37 +7,45 @@ import uuid
 
 class SOSExtractor(Extractor):
    """docstring for BasicExtractor"""
-   def __init__(self, wcs_url, extract_dates, extract_area=None, extract_variable=None, extract_depth=None):
+   def __init__(self, wcs_url, extract_dates, offering, observed_property, feature, extract_area=None, extract_variable=None, extract_depth=None):
       super(SOSExtractor, self).__init__(wcs_url, extract_dates,  extract_area=extract_area, extract_variable=extract_variable, extract_depth=extract_depth)
-      
+      self.offering = offering
+      self.observed_property = observed_property
+      self.feature = feature
+      self.sos_url = wcs_url
+
+
    def getData(self):
+      date_from = self.extract_dates[0]
+      date_to = self.extract_dates[1]
+
       request_data = {
          'service': 'SOS',
          'request': 'GetResult',
          'version': '2.0.0',
-         "offering": "PenleeMet",
-         "observedProperty": "http://mmisw.org/ont/cf/parameter/air_temperature",
-         "featureOfInterest": "penlee",
+         "offering": self.offering,
+         "observedProperty": self.observed_property,
+         "featureOfInterest": self.feature,
          "temporalFilter": [
             {
                "during": {
                "ref": "om:phenomenonTime",
                "value": [
-                  "2014-05-02T11:20:00.000Z",
-                 "2019-08-22T18:00:00.000Z"
-                  ]
+                  date_from,
+                  date_to
+               ]
                }
             }
          ]
       }
       data = json.dumps(request_data)
-
+      print data
       headers = {
          'Accept': 'application/json',
          'Content-Type': 'application/json'
       }
 
-      req = urllib2.Request(self.wcs_url, data, headers)
+      req = urllib2.Request(self.sos_url, data, headers)
       res = urllib2.urlopen(req)
 
       fname = self.outdir+str(uuid.uuid4())+".txt"
