42fc2578fe85b7fea23e72197049e80dcc97d894
Ben Calton <bac@pml.ac.uk>
2016-11-10 09:04:45 +0000

    Update log directory

--- config/global-config-server.js
+++ config/global-config-server.js
@@ -6,5 +6,6 @@ GLOBAL.config = {
    session : {
       secret : 'nxYGob60ibKkeiwfL6ABwLQd3Ulyj9JM4YFHWb555o5Ovq0cx6tDxyoYufT8h7eS0Rxfg5WdZV31iSYKbi1daAD62jr5dL58SVTZhDUle9Azz7UnJIBe7GCAuF09kL2umdhl45110pfZPsXxmlj7cH',
       age : 0
-   }
+   },
+   logDir : 'logs/akzonobel.eofrom.space'
 }
