8bfbea0fe9eb8ab73de716255ed296f7c4fffbaf
Nick Mackenzie <nik@pml.ac.uk>
2016-11-24 14:40:46 +0000

    Escaped ip address.

--- test_dependencies/init.js
+++ test_dependencies/init.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var path = require('path');
 
-var configSource = path.join(__dirname, '../test_dependencies/config/site_settings/127.0.0.1:6789');
-var configDestination = path.join(__dirname, '../config/site_settings/127.0.0.1:6789');
+var configSource = path.join(__dirname, '../test_dependencies/config/site_settings/127.0.0.1\:6789');
+var configDestination = path.join(__dirname, '../config/site_settings/127.0.0.1\:6789');
 
 fs.symlinkSync(configSource, configDestination);
