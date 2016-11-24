410acf5113691a726a5284af8c3a7b4fd090d052
Nick Mackenzie <nik@pml.ac.uk>
2016-11-24 14:58:31 +0000

    Created config/site_settings directory for tests.

--- test_dependencies/init.js
+++ test_dependencies/init.js
@@ -1,5 +1,8 @@
 var fs = require('fs');
+var mkdirp = require('mkdirp');
 var path = require('path');
+
+mkdirp.sync(path.join(__dirname, '../config/site_settings'));
 
 var configSource = path.join(__dirname, '../test_dependencies/config/site_settings/127.0.0.1\:6789');
 var configDestination = path.join(__dirname, '../config/site_settings/127.0.0.1\:6789');
