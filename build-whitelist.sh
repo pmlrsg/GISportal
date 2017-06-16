1115bad97da358b8882da73b65132b298b5288b3
Ben Calton <bac@pml.ac.uk>
2017-06-16 09:19:57 +0100

    Tweaks to follow symlinks, unify sed separators, make executable

--- build-whitelist.sh
+++ build-whitelist.sh
@@ -3,15 +3,15 @@
 # This script will find all domains used by configs and append them to the proxy whitelist.
 # If the whitelist doesn't exist at server startup, this script is run by the server after copying in the default whitelist
 
-find ./config -name "*.json"\
+find -L ./config -name "*.json"\
    -not -name "*overview.json"\
    -not -name "*_walkthrough.json"\
    -not -name "*vectorLayers.json"\
    -not -name "*dictionary.json"\
    -not -name "members.json"\
-   | sed  "s/-.*$//g"\
+   | sed  "s|-.*$||g"\
    | sed "s|.*/||g"\
-   | sed 's/$/\/**/'\
+   | sed 's|$|/**|'\
    | sort\
    | uniq\
    >> ./config/proxy-whitelist.txt
