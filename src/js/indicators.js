2a7b6c88ac550068009a9f7ec69d0bcb0132037c
Benjamin O'Driscoll <bod@pml.ac.uk>
2023-01-23 12:11:27 +0000

    Changed 'compare' to 'map-holder' for the parent holder of both maps

--- src/js/indicators.js
+++ src/js/indicators.js
@@ -238,16 +238,16 @@ gisportal.indicatorsPanel.initDOM = function() {
          return;
       }
       
-      if (document.getElementById('compare').className == 'compare'){
+      if (document.getElementById('map-holder').className == 'compare'){
          // compare_map={};
-         document.getElementById('compare').className = 'view1';
+         document.getElementById('map-holder').className = 'view1';
          var compare_map_=document.getElementById('compare_map');
          compare_map_.innerHTML = '';
          map.updateSize();
       }
       
-      if (document.getElementById('compare').className == 'view1'){
-         document.getElementById('compare').className = 'swipeh';
+      if (document.getElementById('map-holder').className == 'view1'){
+         document.getElementById('map-holder').className = 'swipeh';
          
          // $.notify("Swipe Details:\nMove the slider to the position of interest.\nMove the timeline to update the layer on the RHS.");
          
@@ -279,7 +279,7 @@ gisportal.indicatorsPanel.initDOM = function() {
          var swipe_element=document.getElementsByClassName('ol-swipe');
          // map.removeControl(swipe);
          swipe_element[0].remove();
-         document.getElementById('compare').className = 'view1' ;
+         document.getElementById('map-holder').className = 'view1' ;
          var compare_map_element=document.getElementById('compare_map');
          compare_map_element.innerHTML = '';
          document.getElementById('comparisonDetails').style.display='none';
@@ -299,20 +299,20 @@ gisportal.indicatorsPanel.initDOM = function() {
          return;
       }
 
-      if (document.getElementById('compare').className == 'swipeh'){
+      if (document.getElementById('map-holder').className == 'swipeh'){
          var swipe_element=document.getElementsByClassName('ol-swipe');
          // map.removeControl(swipe);
          swipe_element[0].remove();
          gisportal.unclipMap();
-         document.getElementById('compare').className = 'view1' ;
+         document.getElementById('map-holder').className = 'view1' ;
          var compare_map_element=document.getElementById('compare_map');
          compare_map_element.innerHTML = '';
          // compare_map.updateSize(); // @TODO To be deleted once not required
          map.updateSize(); // @TODO To be deleted once not required
       }
 
-      if (document.getElementById('compare').className == 'view1') {
-         document.getElementById('compare').className = 'compare';
+      if (document.getElementById('map-holder').className == 'view1') {
+         document.getElementById('map-holder').className = 'compare';
          // Synchronise both maps
          gisportal.initialiseSynchronisedMaps();
          
@@ -331,7 +331,7 @@ gisportal.indicatorsPanel.initDOM = function() {
       else {
          // Then go back to original view
          // compare_map={};
-         document.getElementById('compare').className = 'view1';
+         document.getElementById('map-holder').className = 'view1';
          document.getElementById('comparisonDetails').style.display='none';
          var compare_map_=document.getElementById('compare_map');
          compare_map_.innerHTML = '';
@@ -341,7 +341,7 @@ gisportal.indicatorsPanel.initDOM = function() {
       }
    });
    $('.js-exit-compare').on('click', function() {
-      currentView=document.getElementById('compare').className;
+      currentView=document.getElementById('map-holder').className;
 
       // Hide the HUD
       document.getElementById('comparisonDetails').style.display='none';
@@ -404,7 +404,7 @@ gisportal.indicatorsPanel.initDOM = function() {
                }
             });
             // Prevent re-occurence of this message if there is already aa swipe or comparison screen loaded
-            if (warnFlag && document.getElementById('compare').className=='view1'){
+            if (warnFlag && document.getElementById('map-holder').className=='view1'){
                var firstString='The palette and/or colorscale range is different from the defaults set for this layer.\n';
                var secondString='This is not yet supported using this feature so you may notice differences in the styling of your comparison screens';
                $.notify(firstString+secondString);
