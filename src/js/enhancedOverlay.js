55bb9c4692c6cf2886b5d14909f85b8075f57284
Benjamin O'Driscoll <bod@pml.ac.uk>
2023-06-13 13:54:04 +0100

    Gif is now cached only after user hits the overlay button
    Styling for overlay moved out of _compare.scss into appropriately named new file
    Overlay resized to include sidebar

--- src/js/enhancedOverlay.js
+++ src/js/enhancedOverlay.js
@@ -15,25 +15,31 @@ gisportal.enhancedOverlay.initDOM=function(){
       // Unhide the tab at the top
       document.getElementById('overlay-animations').className='js-show-panel tab';
     }
-
+    
     // Display the calendar widget with dates disabled: https://stackoverflow.com/questions/15400775/jquery-ui-datepicker-disable-array-of-dates
     $("#datepicker").datepicker({
       showButtonPanel: true
     });
-
+    
     $('.js-overlay').on('click', gisportal.overlayGIF);
     
 };
 
 gisportal.overlayGIF=function(){
-    // Construct the gif_overlay 
-    var pos = ol.proj.fromLonLat([-1.97, 54.6]);
-    var gif_overlay = new ol.Overlay({
-      position: pos,
-      positioning: 'center-center',
-      element: document.getElementById('gif-overlay'),
-      stopEvent: false
-    });
+  // Construct the gif_overlay 
+  var pos = ol.proj.fromLonLat([-1.97, 54.6]);
+  var gif_overlay = new ol.Overlay({
+    position: pos,
+    positioning: 'center-center',
+    element: document.getElementById('gif-overlay'),
+    stopEvent: false
+  });
+  
+  map.addOverlay(gif_overlay);
+  document.getElementById('gif-overlay').style.display='block';
+  document.getElementById('gif-overlay').style.background='url("../../static/movie.gif") no-repeat scroll 0% 0% transparent';
+  document.getElementById('gif-overlay').style.width='668px';
+  document.getElementById('gif-overlay').style.height='631px';
+  document.getElementById('gif-overlay').style.backgroundSize='cover';
 
-      map.addOverlay(gif_overlay);
 };
