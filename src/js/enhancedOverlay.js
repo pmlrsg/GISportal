6804f90e14c1b996762bcd86f5f6922db27ae610
Benjamin O'Driscoll <bod@pml.ac.uk>
2023-06-22 08:40:03 +0100

    Calendar populated with available dates

--- src/js/enhancedOverlay.js
+++ src/js/enhancedOverlay.js
@@ -15,14 +15,12 @@ gisportal.enhancedOverlay.initDOM=function(){
       // Unhide the tab at the top
       document.getElementById('overlay-animations').className='js-show-panel tab';
     }
+    gisportal.enhancedOverlay.gifList=null;
     
     // Populate the available dates
-    gisportal.populateAvailableOverlays();
+    gisportal.enhancedOverlay.populateAvailableOverlays();
     
-    // Display the calendar widget with dates disabled: https://stackoverflow.com/questions/15400775/jquery-ui-datepicker-disable-array-of-dates
-    $("#datepicker").datepicker({
-      showButtonPanel: true
-    });
+    gisportal.enhancedOverlay.waitForPopulatedOverlays(0);
     
     $('.js-overlay').on('click', gisportal.overlayGIF);
     
@@ -56,8 +54,7 @@ gisportal.overlayGIF=function(){
   document.getElementById('gif-overlay').style.backgroundSize='cover';
 
 };
-
-gisportal.populateAvailableOverlays = function(){
+gisportal.enhancedOverlay.populateAvailableOverlays = function(){
   // A request to populate the dropdown with the shared polygons
   $.ajax({
      url:  gisportal.middlewarePath + '/settings/get_enhanced_overlays',
@@ -92,9 +89,96 @@ gisportal.populateAvailableOverlays = function(){
         console.log('Number of chlFiles: ',chlFiles.length);
         console.log('Number of others: ',otherFiles.length);
 
+        gisportal.enhancedOverlay.directoryTop=directoryTop;
+        gisportal.enhancedOverlay.gifList=gifList;
+        
+        gisportal.enhancedOverlay.rgb={};
+        gisportal.enhancedOverlay.chl={};
+
+        gisportal.enhancedOverlay.rgb.rgbDates=rgbDates;
+        gisportal.enhancedOverlay.chl.chlDates=chlDates;
+
+        var dateResultRGB=gisportal.enhancedOverlay.findEarliestLatestAndMissingDates(rgbDates);
+        var dateResultChl=gisportal.enhancedOverlay.findEarliestLatestAndMissingDates(chlDates);
+
+        gisportal.enhancedOverlay.rgb.missingRGB=dateResultRGB.missing;
+        gisportal.enhancedOverlay.rgb.earliest=dateResultRGB.earliest;
+        gisportal.enhancedOverlay.rgb.latest=dateResultRGB.latest;
+        gisportal.enhancedOverlay.rgb.missingRGB=dateResultChl.missing;
+        gisportal.enhancedOverlay.rgb.earliest=dateResultChl.earliest;
+        gisportal.enhancedOverlay.rgb.latest=dateResultChl.latest;
+
+
      },
      error: function(e){
         console.log('Something went wrong');
      }
   });
 };
+
+gisportal.enhancedOverlay.findEarliestLatestAndMissingDates=function(dates) {
+  // Convert string dates to Date objects
+  var dateObjects = dates.map(function(dateStr) {
+    return new Date(dateStr);
+  });
+
+  // Find the earliest and latest dates
+  var earliestDate = new Date(Math.min.apply(null, dateObjects));
+  var latestDate = new Date(Math.max.apply(null, dateObjects));
+
+  // Generate an array of all dates between the earliest and latest dates
+  var allDates = [];
+  var currentDate = new Date(earliestDate);
+  while (currentDate <= latestDate) {
+    allDates.push(new Date(currentDate));
+    currentDate.setDate(currentDate.getDate() + 1);
+  }
+
+  // Convert the input dates and all dates to strings (e.g., "YYYY-MM-DD")
+  var inputDateStrings = dates;
+  var allDateString = allDates.map(function(date) {
+    return date.toISOString().split('T')[0];
+  });
+
+  // Find the missing dates by comparing the input dates with all dates
+  var missingDates = allDateString.filter(function(date) {
+    return !inputDateStrings.includes(date);
+  });
+
+  // Format the dates as strings (e.g., "YYYY-MM-DD")
+  var earliestDateString = earliestDate.toISOString().split('T')[0];
+  var latestDateString = latestDate.toISOString().split('T')[0];
+
+  // Return the earliest, latest, and missing dates
+  return {
+    earliest: earliestDateString,
+    latest: latestDateString,
+    missing: missingDates
+  };
+};
+
+gisportal.enhancedOverlay.waitForPopulatedOverlays=function(counter){
+  if (counter>8){
+    return;
+  }
+
+  if (gisportal.enhancedOverlay.gifList===null){
+    setTimeout(function(){
+      counter=counter+1;
+      gisportal.enhancedOverlay.waitForPopulatedOverlays(counter);
+    },1000);
+  }
+  
+  else{
+      $("#datepicker").datepicker({
+        showButtonPanel: true,
+        minDate:new Date(gisportal.enhancedOverlay.rgb.earliest),
+        maxDate:new Date(gisportal.enhancedOverlay.rgb.latest),
+        beforeShowDay: function(date){
+          var string = $.datepicker.formatDate('yy-mm-dd', date);
+          return [ gisportal.enhancedOverlay.rgb.missingRGB.indexOf(string) == -1 ];
+        }
+      });
+  }
+};
+  
