5879247987462ea21305035e65a892016ce521d5
bod <bod@pml.ac.uk>
2024-01-02 10:06:50 +0000

    Removed bug preventing legend from loading

--- src/js/scalebar.js
+++ src/js/scalebar.js
@@ -32,6 +32,7 @@ gisportal.scalebars.getScalebarDetails = function(id)  {
       }   
       // If the url is still null then there were no matches, so use a generic url
       if(url === null)
+      console.log('Legend Loaded');
          url = gisportal.scalebars.createGetLegendURL(indicator, "");
      
       
