3442b0a969644a2118b8ae88d1b1abd6a6b89533
bod <bod@pml.ac.uk>
2025-03-07 12:20:49 +0000

    Revert "Fixed the issue of Geoserver layers not loading"
    This reverts commit 39cb8331a4c3f3b9732cb2c3b6924195ead2aaff.

--- src/js/scalebar.js
+++ src/js/scalebar.js
@@ -29,19 +29,9 @@ gisportal.scalebars.getScalebarDetails = function(id)  {
                return false; // Break loop
             }
          });
-      }
-      else{
-         // Taking action here to try and resolve this issue of scalebars not loading. Added this else clause and additional if statement below.
-         // If we have issues in future of scalebars failing to load or update we need to look here in the first instance.  
-         url = gisportal.scalebars.createGetLegendURL(indicator, "");
-      }
-
-      try{
-         if (indicator.serverName.includes('geoserver')){
-            // For some reason we always need to run this to satisfy legends coming from geoserver. 
-            url = gisportal.scalebars.createGetLegendURL(indicator, "");
-         }
-      }catch(e){}
+      }   
+      // This should only be set if the above fails but we are always executing this line DSN, PETWA, BOD
+      url = gisportal.scalebars.createGetLegendURL(indicator, "");
      
       
       // Set the scalebar inputs to be correct 
